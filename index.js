d9514c4a1648c029101ea8015cfba4a9c70d1fb1
Ana Chicaiza <anachicaiza@icloud.com>
2026-03-08 21:44:30 +0000

    Joke app changed to poem app
    function response takes all data from AI.
    
    function displayShecodesPoem display a inicial poem when the app is loaded.

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-function displayJoke(response) {
-    console.log(response.data);
-    let jokeElement = document.querySelector("#joke");
-    jokeElement.innerHTML = response.data.answer;
+function displayPoem(response) {
 
-    new Typewriter("#joke", {
+    new Typewriter("#poem", {
         strings: response.data.answer,
         autoStart: true,
         cursor: null,
         delay: 20
-    })
+    });
+
+    let submitButton = document.querySelector('#submit-button')
+    submitButton.removeAttribute('disabled');
 }
 
-function generateJoke(event) {
+function displayShecodesPoem() {
+    let poem = `
+    My heart compiles when your logic is near,<br />
+    Through loops of thought, your patterns appear,<br />
+    In silent code our dreams align,<br />
+    A love algorithm—yours and mine.<br />
+    <strong>SheCodes AI</strong>
+    `;
+
+    new Typewriter("#poem", {
+        strings: poem,
+        autoStart: true,
+        delay: 1,
+    });
+
+}
+
+function generate(event) {
     event.preventDefault();
     let apiKey = "b739b64actfb7710ab2aa8f6044o4c38";
-    let context = "You are a funny AI assisstant who tells really good jokes. Everyitme the button is clicked i want you to tell a different joke. The joke must be provided in HTML format. Example: <p>This is a joke<p>";
-    let prompt = "Generate a short joke about whatever you want.";
+    let context =
+        "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning. Do not include markdown around it";
+    let instructionsInput = document.querySelector("#instructions");
+    let prompt = `User instructions: Generate a romantic poem about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-    console.log("Generating...")
-    let jokeElement = document.querySelector("#joke");
-    jokeElement.innerHTML = "Generating a joke for you...please wait";
-    axios.get(apiURL).then(displayJoke);
+    let submitButton = document.querySelector('#submit-button')
+    submitButton.setAttribute('disabled', 'disabled');
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden")
+    poemElement.innerHTML = `<div class="waiting">⏳ Generating a poem about ${instructionsInput.value}..</div>`;
+    axios.get(apiURL).then(displayPoem);
 }
 
-let button = document.querySelector("#joke-button");
-button.addEventListener("click", generateJoke);
+let poemForm = document.querySelector("#search-form");
+poemForm.addEventListener("submit", generate);
+displayShecodesPoem();
